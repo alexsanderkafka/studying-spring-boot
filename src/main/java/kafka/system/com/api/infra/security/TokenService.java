5ea140b3fb6a59f990b913c1fbc17f50a9690115
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2024-02-24 17:20:29 -0300

    Utilizando a biblioteca auth0 para criar token JWT

---
+++ src/main/java/kafka/system/com/api/infra/security/TokenService.java
@@ -0,0 +1,36 @@
+package kafka.system.com.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import kafka.system.com.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario){
+        try {
+            var alroritmo = Algorithm.HMAC256("12345678");
+
+            return JWT.create()
+                    .withIssuer("API Kafka.system")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(alroritmo);
+
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
